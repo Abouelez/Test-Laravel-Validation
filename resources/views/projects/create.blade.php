f9f04455826087c9a93942bafcfc17bda835795c
Ezz690 <abouelez10@gmail.com>
2023-02-21 13:34:50 +0200

    all passed

--- resources/views/projects/create.blade.php
+++ resources/views/projects/create.blade.php
@@ -6,12 +6,20 @@
 
 <form method="POST" action="{{ route('projects.store') }}">
     @csrf
+    @if ($errors->any())
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    @endif
     Title:
     <br />
     <input type="text" name="title" />
     <br /><br />
     Description:
     <br />
+
     <input type="text" name="description" />
     <br /><br />
     <button type="submit">Save</button>
